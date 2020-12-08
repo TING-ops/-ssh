23cda56acc2beb36043cae94a0fda26481d0d19c
TING-ops <robindingwei@sina.cn>
2020-12-08 11:30:34 +0800

    添加擦除案例,刮刮乐效果

---
+++ CaiChu/Assets/2/Materials/Line.shader
@@ -0,0 +1,34 @@
+﻿Shader "Unlit/Line"
+{
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" "Queue" = "Transparent"}
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+			
+			v2f vert (appdata_base v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				return fixed4(0,0,0,0);
+			}
+			ENDCG
+		}
+	}
+}
---
+++ CaiChu/Assets/2/Scripts/DrawManager.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DrawManager : MonoBehaviour
+{
+
+    public LineRenderer Line;
+
+    /// <summary>
+    /// 恢复速度
+    /// </summary>
+    [SerializeField]
+    [Range(10,50f)]
+    private float recorverySpeed = 10;
+
+    /// <summary>
+    /// 最小检测距离，距离越小精度越好
+    /// </summary>
+    [SerializeField]
+    [Range(0.01f, 0.3f)]
+    private float minDetectedDistance = 0.2f;
+
+    [SerializeField]
+    [Range(0,0.5f)]
+    private float lineWidth = 0.2f;
+
+    private Transform lineFather;
+    private LineRenderer currentLine;
+    private Queue<LineRenderer> lineQueue = new Queue<LineRenderer>();
+    void Awake()
+    {
+        lineFather = GameObject.Find("Lines").transform;
+    }
+
+    void Start()
+    {
+     
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CreateLine();
+        }
+        else if (Input.GetMouseButton(0) && currentLine)
+        {
+            ControlLine();
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            ClearLine();
+        }
+    }
+
+    private void CreateLine()
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5));
+        currentLine = Instantiate(Line,lineFather);
+        currentLine.name = "Line";
+        lineQueue.Enqueue(currentLine);
+        SetLinePosition(pos);
+        currentLine.startWidth = lineWidth;
+        currentLine.endWidth = lineWidth;
+    }
+
+    private Vector3 lastPos;
+    private void ControlLine()
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,5));
+        if(Vector3.Distance(lastPos,pos) >= minDetectedDistance)
+        {
+            SetLinePosition(pos);
+            lastPos = pos;
+        }
+
+    }
+    private void ClearLine()
+    {
+        currentLine = null;
+        StartCoroutine(BackwardRecorvery());
+    }
+
+    private LineRenderer lineTemp = null;
+    private List<Vector3> positionList = new List<Vector3>();
+    IEnumerator ForwardRecorvery()
+    {
+        if (lineTemp == null && lineQueue.Count > 0)
+        {
+
+            lineTemp = lineQueue.Dequeue();
+            int count = lineTemp.positionCount;
+
+            for (int i = 0; i < lineTemp.positionCount; i++)
+            {
+                positionList.Add(lineTemp.GetPosition(i));
+            }
+
+            float timer = 0;
+            while (lineTemp.positionCount > 0)
+            {
+                if (timer < 0.2f)
+                {
+                    timer += Time.fixedDeltaTime * recorverySpeed;
+                }
+                else
+                {
+                    if (count > 0)
+                    {
+                        positionList.RemoveAt(0);
+                        lineTemp.positionCount = positionList.Count;
+                        for (int i = 0; i < positionList.Count; i++)
+                        {
+                            lineTemp.SetPosition(i, positionList[i]);
+                        }
+                    }
+                    timer = 0;
+                }
+                yield return null;
+            }
+
+            lineTemp = null;
+            StartCoroutine(ForwardRecorvery());
+
+        }
+
+    }
+    IEnumerator BackwardRecorvery()
+    {
+        if (lineTemp == null && lineQueue.Count > 0)
+        {
+
+            lineTemp = lineQueue.Dequeue();
+            int count = lineTemp.positionCount;
+
+            float timer = 0;
+            while (lineTemp.positionCount > 0)
+            {
+                if (timer < 0.2f)
+                {
+                    timer += Time.fixedDeltaTime * recorverySpeed;
+                }
+                else
+                {
+                    if (count > 0)
+                    {
+                        lineTemp.positionCount = --count;
+                    }
+                    else
+                    {
+                        lineTemp.positionCount = 0;
+                    }
+                    timer = 0;
+                }
+                yield return null;
+            }
+
+            lineTemp.startWidth = 0;
+            lineTemp.endWidth = 0;
+            lineTemp = null;
+            StartCoroutine(BackwardRecorvery());
+
+        }
+
+    }
+    private void SetLinePosition(Vector3 pos)
+    {
+        int positionCount = currentLine.positionCount;
+        currentLine.positionCount += 1;
+        currentLine.SetPosition(positionCount, pos);
+    }
+
+}
